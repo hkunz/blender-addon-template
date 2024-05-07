39b7a7880ea30435eabd2b582b1f57ad7994796c
Harry Kunz <harry.kunz@ymail.com>
2024-05-07 20:56:30 +0800

    add samples to aexpose any modifier property from the GN modifier

--- ui/sidebar_menu.py
+++ ui/sidebar_menu.py
@@ -12,6 +12,19 @@ from {{ADDON_NAME_PACKAGE}}.utils.utils import Utils # type: ignore
 from {{ADDON_NAME_PACKAGE}}.utils.object_utils import ObjectUtils # type: ignore
 from {{ADDON_NAME_PACKAGE}}.utils.icons_manager import IconsManager  # type: ignore
 
+IDNAME_ICONS = {
+    "NodeSocketMaterial": "MATERIAL_DATA",
+    "NodeSocketCollection": "OUTLINER_COLLECTION",
+    "NodeSocketTexture": "TEXTURE_DATA",
+    "NodeSocketImage": "IMAGE_DATA",
+}
+IDNAME_TYPE = {
+    "NodeSocketMaterial": "materials",
+    "NodeSocketCollection": "collections",
+    "NodeSocketTexture": "textures",
+    "NodeSocketImage": "images",
+}
+
 @persistent
 def on_depsgraph_update(scene, depsgraph=None):
     context = bpy.context
@@ -20,10 +33,10 @@ def on_depsgraph_update(scene, depsgraph=None):
     obj = context.active_object
     if not obj:
         return
-    properties: MyPropertyGroup = context.scene.my_property_group_pointer # type: ignore
+    properties: MyPropertyGroup1 = context.scene.my_property_group_pointer # type: ignore
     check_object_selection_change(context, properties, obj)
 
-def my_settings_callback(self: bpy.types.Scene, context: bpy_types.Context) -> List[Tuple[str, str, str]]:
+def my_sample_settings_callback(self: bpy.types.Scene, context: bpy_types.Context) -> List[Tuple[str, str, str]]:
     SAMPLE_LIST: List[Tuple[str, str, str]] = [
         ("NONE", "None", "Item Description"),
         ("OPT1", "Option 1", "Item Description for Option 1"),
@@ -31,18 +44,24 @@ def my_settings_callback(self: bpy.types.Scene, context: bpy_types.Context) -> L
     ]
     return SAMPLE_LIST
 
+def my_sample_update_rgb_nodes(self, context):
+    mat = self.id_data
+    nodes = [n for n in mat.node_tree.nodes if isinstance(n, bpy.types.ShaderNodeRGB)]
+    for n in nodes:
+        n.outputs[0].default_value = self.rgb_controller
+
 def check_object_selection_change(context, properties, obj):
     # if PREVIOUS_ACTIVE_OBJECT == obj:
     #     return
     # PREVIOUS_ACTIVE_OBJECT = obj
     pass
 
-class MyPropertyGroup(bpy.types.PropertyGroup):
+class MyPropertyGroup1(bpy.types.PropertyGroup):
 
     my_enum_prop: bpy.props.EnumProperty(
         name="My Enum Prop",
         description="My enum prop description",
-        items=my_settings_callback,
+        items=my_sample_settings_callback,
         #default="NONE", # cannot set a default when using dynamic EnumProperty
     ) # type: ignore https://blender.stackexchange.com/questions/311578/how-do-you-correctly-add-ui-elements-to-adhere-to-the-typing-spec/311770#311770
 
@@ -75,6 +94,18 @@ class MyPropertyGroup(bpy.types.PropertyGroup):
         subtype='FILE_PATH'
     ) # type: ignore
 
+
+class MyPropertyGroup2(bpy.types.PropertyGroup):
+    rgb_controller: bpy.props.FloatVectorProperty(
+        name="Diffuse color",
+        subtype='COLOR',
+        default=(1, 1, 1, 1),
+        size=4,
+        min=0, max=1,
+        description="color picker",
+        update = my_sample_update_rgb_nodes
+    ) # type: ignore
+
 class OBJECT_PT_my_addon_panel(bpy.types.Panel):
     bl_label = f"{{ADDON_NAME}} {Utils.get_addon_version()}"
     bl_space_type = 'VIEW_3D'
@@ -85,16 +116,34 @@ class OBJECT_PT_my_addon_panel(bpy.types.Panel):
         layout: bpy.types.UILayout = self.layout
         selected_mesh_objects = [obj for obj in context.selected_objects if obj.type == 'MESH']
         active_object = context.active_object if len(selected_mesh_objects) > 0 and context.active_object in selected_mesh_objects else None
-        properties: MyPropertyGroup = context.scene.my_property_group_pointer
+        properties: MyPropertyGroup1 = context.scene.my_property_group_pointer
         box = layout.box().column()
+
+        # sample props:
         box.prop(properties, "my_enum_prop")
         box.prop(properties, "my_float_prop")
         box.prop(properties, "my_string_prop")
         box.prop(properties, "my_file_input_prop")
-        box.label(text="Icon Label", icon=IconsManager.BUILTIN_ICON_MESH_DATA)
-        self.draw_expanded_options(context, layout)
 
-    def draw_expanded_options(self, context, layout):
+        box.label(text="Icon Label", icon=IconsManager.BUILTIN_ICON_MESH_DATA)
+
+        self.draw_sample_modifier_exposed_props(context, layout, "GeometryNodes")
+        self.draw_sample_expanded_options(context, layout)
+        self.draw_sample_color_picker(context, layout)
+
+    def draw_sample_modifier_exposed_props(self, context, layout, md_name = "GeometryNodes"):
+        # to test this function add a Geometry Nodes Modifier by the name "GeometryNodes" and add some inputs to it which will get exposed in the addon panel
+        # https://blender.stackexchange.com/questions/317739/unable-to-access-exposed-material-input-in-addon-from-geometry-nodes-modifier
+        ob = context.object
+        if not hasattr(ob, "modifiers") or md_name not in ob.modifiers:
+            return
+        md = ob.modifiers[md_name]
+        if md.type == "NODES" and md.node_group:
+            for rna in md.node_group.interface.items_tree:
+                if hasattr(rna, "in_out") and rna.in_out == "INPUT":
+                    self.add_layout_gn_prop_pointer(layout, md, rna)
+
+    def draw_sample_expanded_options(self, context, layout):
         ebox = layout.box()
         row = ebox.box().row()
         row.prop(
@@ -109,18 +158,40 @@ class OBJECT_PT_my_addon_panel(bpy.types.Panel):
             #self.add_layout_gn_prop(layout, context.object.modifiers["Geometry Nodes"], "Socket_2") # https://blender.stackexchange.com/questions/317571/how-can-i-expose-geometry-nodes-properties-in-my-addon-panel/317586
             col.operator(EXPORT_OT_file_vox.bl_idname, text="Export Button")
 
+    def draw_sample_color_picker(self, context, layout):
+        ob = context.object
+        mat = ob.active_material
+        layout.prop(mat.my_slot_setting, "rgb_controller")
+        # sample to set the color via python:
+        # ob.active_material.slot_setting.rgb_controller = (1, 0, 0, 1)
+
     def add_layout_gn_prop(self, layout, modifier, prop_id):
         name = ObjectUtils.get_modifier_prop_name(modifier, prop_id)
         layout.prop(data=modifier, property=f'["{prop_id}"]', text=name)
+
+    def add_layout_gn_prop_pointer(self, layout, md, rna): # Need to ensure that md and identifier are correct
+        if rna.bl_socket_idname == "NodeSocketGeometry":
+            return
+        if rna.bl_socket_idname in IDNAME_ICONS:
+            layout.prop_search(md, f'["{rna.identifier}"]',
+                search_data = bpy.data,
+                search_property = IDNAME_TYPE[rna.bl_socket_idname],
+                icon = IDNAME_ICONS[rna.bl_socket_idname],
+                text = "My " + rna.name
+            )
+        else:
+            layout.prop(md, f'["{rna.identifier}"]', text=rna.name)
 
     @classmethod
     def poll(cls, context):
         return True
 
 def register() -> None:
-    bpy.utils.register_class(MyPropertyGroup)
     bpy.utils.register_class(OBJECT_PT_my_addon_panel)
-    bpy.types.Scene.my_property_group_pointer = bpy.props.PointerProperty(type=MyPropertyGroup)
+    bpy.utils.register_class(MyPropertyGroup1)
+    bpy.utils.register_class(MyPropertyGroup2)
+    bpy.types.Material.my_slot_setting = bpy.props.PointerProperty(type=MyPropertyGroup2)
+    bpy.types.Scene.my_property_group_pointer = bpy.props.PointerProperty(type=MyPropertyGroup1)
     bpy.types.Scene.expanded_options = bpy.props.BoolProperty(default=False)
     bpy.utils.register_class(OBJECT_OT_OperatorEmpty)
     bpy.utils.register_class(EXPORT_OT_file_vox) # sample file export operator resulting in open dialog
@@ -129,8 +200,10 @@ def register() -> None:
     bpy.app.handlers.depsgraph_update_post.append(on_depsgraph_update)
 
 def unregister() -> None:
-    bpy.utils.unregister_class(MyPropertyGroup)
     bpy.utils.unregister_class(OBJECT_PT_my_addon_panel)
+    bpy.utils.unregister_class(MyPropertyGroup1)
+    bpy.utils.unregister_class(MyPropertyGroup2)
+    del bpy.types.Material.my_slot_setting
     del bpy.types.Scene.expanded_options
     del bpy.types.Scene.my_property_group_pointer
     bpy.utils.unregister_class(OBJECT_OT_OperatorEmpty)
