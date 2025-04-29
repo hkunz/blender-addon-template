fdf2a05b97fa77468010abb7891a2d9acfd8c6dc
Harry McKenzie <harry.kunz@ymail.com>
2025-04-29 16:57:04 +0800

    fixed accidental commit change

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -128,9 +128,9 @@ fi
 # ────────────────────────────────────────────────────────────────────────────────
 # Replace placeholders
 # ────────────────────────────────────────────────────────────────────────────────
-replace_package="a"
-replace_addon_name="a"
-replace_addon_name_full="a"
+replace_package="{{ADDON_NAME_PACKAGE}}"
+replace_addon_name="{{ADDON_NAME}}"
+replace_addon_name_full="{{ADDON_NAME_FULL}}"
 
 perform_replacements() {
     local file="$1"
